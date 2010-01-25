4e876b59745a67cf1fbcbaacf1ca1675c3e1946a
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-25 19:38:29 +0100

    Add validation and usage logics into the model

--- onetime/models.py
+++ onetime/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,4 +10,19 @@ class Key(models.Model):
     usage_left = models.IntegerField(null=True, default=1)
     expires = models.DateTimeField(null=True)
     next = models.CharField(null=True, max_length=200)
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.key, self.user.username)
+
+    def is_valid(self):
+        if self.usage_left is not None and self.usage_left <= 0:
+            return False
+        if self.expires is not None and self.expires < datetime.now():
+            return False
+        return True
+
+    def update_usage(self):
+        if self.usage_left is not None:
+            self.usage_left -= 1
+            self.save()
 
