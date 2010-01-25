a1c120c4d296f77218893f78096f5b8bc410557f
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-25 19:37:47 +0100

    Add created field

--- onetime/models.py
+++ onetime/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Key(models.Model):
     user = models.ForeignKey(User)
     key = models.CharField(max_length=40)
+    created = models.DateTimeField(auto_now_add=True)
     usage_left = models.IntegerField(null=True, default=1)
     expires = models.DateTimeField(null=True)
     next = models.CharField(null=True, max_length=200)
