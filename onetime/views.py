c3e7b563c3eeb24aa269f23672b8f469470908b7
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-25 17:05:00 +0100

    Add an option to redirect user to a page if the key is already expired.

--- onetime/views.py
+++ onetime/views.py
@@ -11,13 +11,20 @@ from onetime.models import Key
 def cleanup(request):
     utils.cleanup()
 
-def login(request, key):
+def login(request, key, redirect_expired_to=None):
     data = get_object_or_404(Key, key=key)
 
+    expired = False
     if data.usage_left is not None and data.usage_left == 0:
-        return HttpResponseGone()
+        expired = True
     if data.expires is not None and data.expires < datetime.now():
-        return HttpResponseGone()
+        expired = True
+
+    if expired:
+        if redirect_expired_to is not None:
+            return HttpResponseRedirect(redirect_expired_to)
+        else:
+            return HttpResponseGone()
 
     if data.usage_left is not None:
         data.usage_left -= 1
