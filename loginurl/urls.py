306c19582b6dcbb2356defaa1d38467d6b71f391
Fajran Iman Rusadi <fajran@gmail.com>
2010-03-04 10:21:03 +0100

    Redirect to the default log in page when opening the loginurl page without any key.

--- loginurl/urls.py
+++ loginurl/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
+from django.conf import settings
 
 from loginurl.views import cleanup, login
 
 urlpatterns = patterns('',
     (r'^cleanup/$', cleanup),
     (r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login), 
-    (r'^$', redirect_to, {'url': None}),
+    (r'^$', redirect_to, {'url': settings.LOGIN_URL}),
 )
 
